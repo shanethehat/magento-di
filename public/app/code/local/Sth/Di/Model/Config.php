f81d76ca2eb8ca8e529c0d8e44719665cddc4c00
Shane Auckland <shane.auckland@gmail.com>
2014-10-23 21:06:50 +0100

    Override core config model

--- public/app/code/local/Sth/Di/Model/Config.php
+++ public/app/code/local/Sth/Di/Model/Config.php
@@ -1,6 +1,14 @@
 <?php
 
-class Sth_Di_Model_Config extends Mage_Core_Model_Abstract
+class Sth_Di_Model_Config extends Mage_Core_Model_Config
 {
-
+    /**
+     * Class construct
+     *
+     * @param mixed $sourceData
+     */
+    public function __construct($sourceData = null)
+    {
+        parent::__construct($sourceData);
+    }
 }
--- public/index.php
+++ public/index.php
@@ -84,4 +84,4 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
-Mage::run($mageRunCode, $mageRunType);
+Mage::run($mageRunCode, $mageRunType, array('config_model' => 'Sth_Di_Model_Config'));

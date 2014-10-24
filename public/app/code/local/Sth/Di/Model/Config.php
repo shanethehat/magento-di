6ccbe983c8be1cb0932f88135a6ee76b7448cb5d
Shane Auckland <shane.auckland@gmail.com>
2014-10-24 08:54:45 +0100

    Add Symfony DI container

--- public/app/code/local/Sth/Di/Model/Config.php
+++ public/app/code/local/Sth/Di/Model/Config.php
@@ -1,7 +1,16 @@
 <?php
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
 class Sth_Di_Model_Config extends Mage_Core_Model_Config
 {
+    /**
+     * @var ContainerBuilder
+     */
+    private $_container;
+
     /**
      * Class construct
      *
@@ -10,5 +19,30 @@ class Sth_Di_Model_Config extends Mage_Core_Model_Config
     public function __construct($sourceData = null)
     {
         parent::__construct($sourceData);
+    }
+
+    /**
+     * Gets an object from the DI Container
+     *
+     * @param string $reference
+     * @return mixed
+     */
+    public function getFromContainer($reference)
+    {
+        if (null === $this->_container) {
+            $this->_initContainer();
+        }
+
+        return $this->_container->get($reference, ContainerBuilder::NULL_ON_INVALID_REFERENCE);
+    }
+
+    /**
+     * Initialises the DI container
+     */
+    private function _initContainer()
+    {
+        $this->_container = new ContainerBuilder();
+        $loader = new XmlFileLoader($this->_container, new FileLocator($this->getOptions()->getEtcDir()));
+        $loader->load('services.xml');
     }
 }
